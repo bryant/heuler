f800e837f6bf43ca3171327ba1e3f01d1467e762
bryant <bryant@nfkb>
2013-08-21 23:10:01 -0400

    improve 002: use even subsequence.

--- 002.hs
+++ 002.hs
@@ -1,2 +1,2 @@
-fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
-main = putStrLn . show . sum $ filter even (takeWhile (< 4000000) fibs)
+evenFibs = 0 : 2 : zipWith (\n m -> n + 4*m) evenFibs (tail evenFibs)
+main = putStrLn . show . sum $ takeWhile (< 4000000) evenFibs
